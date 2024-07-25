914e502a3efa366eb6c98e5bd5b3927525d2482c
ShivaSingh17121997 <shivasingh001997@gmail.com>
2024-07-25 12:02:42 +0530

    dedda

---
+++ Day16/crud operation/README.md
@@ -0,0 +1,5 @@
+// create form compoent send data to the json server / backend
+
+// get the data form api shown into the ui 
+
+CRUD create and read is done;
---
+++ Day17/crud 2/src/Pages/Home.jsx
@@ -0,0 +1,114 @@
+import React from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react'
+
+export default function Home() {
+
+    const [studentData, setStudentData] = useState([]);
+
+    const [name, setName] = useState("")
+
+    const [age, setAge] = useState("")
+
+    const [isMarried, setIsMarried] = useState(false);
+
+    console.log(name, age, isMarried)
+
+
+    // get 
+
+    const fetchedData = () => {
+        fetch("http://localhost:8000/students")
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data)
+                setStudentData(data)
+            })
+            .catch((err) => { console.log("data not found") })
+    }
+
+    useEffect(() => {
+        fetchedData()
+    }, [])
+
+
+
+    // post data in jsonserver
+
+    const handleAddData = (e) => {
+        e.preventDefault();
+
+        fetch("http://localhost:8000/students", {
+            method: "POST",
+            body: JSON.stringify({
+                name: name,
+                age: age,
+                isMarried: isMarried
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        })
+            .then(res => {
+                console.log(res)
+                alert("data added successfully")
+                fetchedData()
+
+            })
+
+        setName("")
+        setAge("")
+        setIsMarried(false)
+
+    }
+
+
+
+    return (
+        <div>
+            {/* adding the data */}
+            <div>
+                <form onSubmit={handleAddData} >
+                    <input type="text" placeholder='enter name' value={name} onChange={(e) => setName(e.target.value)} /> <br /><br />
+
+
+                    <input type="number" placeholder='enter age' value={age} onChange={(e) => setAge(e.target.value)} /><br /><br />
+
+                    <label >
+                        <input type="checkbox" checked={isMarried} onChange={() => setIsMarried(!isMarried)} />
+                        Married
+                    </label> <br />
+                    <button>Add</button>
+
+
+                </form>
+
+            </div>
+            <hr />
+
+
+
+
+
+
+
+
+
+
+            {/* getting the data */}
+            <div>
+                {
+                    studentData.map((item) => {
+                        return <div>
+                            <p>id:{item.id}</p>
+                            <h3>name: {item.name}</h3>
+                            <p>age :{item.age}</p>
+                            <p>Marrid: {item.isMarried ? "Married" : "Unmarried"}</p>
+
+                        </div>
+                    })
+                }
+            </div>
+        </div>
+    )
+}
